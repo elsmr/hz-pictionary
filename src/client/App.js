1e37b8f28d5a0d457e462d8d549d8b8b4fcf058e
Elias Meire <me@eliasmei.re>
2017-03-10 16:45:10 +0100

    remove boilerplate, linter settings

---
+++ src/client/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from 'grommet';
+
+const App = () => (
+  <div className="App">
+    <Title>Test</Title>
+    <span className="test">TEST</span>
+  </div>
+);
+
+export default App;
