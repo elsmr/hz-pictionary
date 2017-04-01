1bac66827ea4ae711597267062969f0f5950ede1
Elias Meire <me@eliasmei.re>
2017-04-01 19:39:47 +0200

    link redux to react

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './containers/App/App';
 import './index.scss';
+import store from './redux/store';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
