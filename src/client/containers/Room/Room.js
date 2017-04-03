0ce1cb285682c60f2b6308b7aaca7531f84da316
Elias Meire <me@eliasmei.re>
2017-04-03 03:09:03 +0200

    Room component

--- src/client/containers/Room/Room.js
+++ src/client/containers/Room/Room.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startWatchingRoom, stopWatchingRoom } from '../../redux/actions';
 
-const Room = () => (
-  <div>
-    Room
-  </div>
-);
+class Room extends React.Component {
+  constructor(props) {
+    super(props);
+    const { startWatchingRoom, match: { params: { roomId } } } = props;
+    startWatchingRoom(roomId);
+  }
 
-export default Room;
+  componentWillUnmount() {
+    const { stopWatchingRoom, match: { params: { roomId } } } = this.props;
+    stopWatchingRoom(roomId);
+  }
+
+  render() {
+    const { match: { params: { roomId } } } = this.props;
+    return (
+      <div>
+        Room: { roomId }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ room: state.room });
+
+export default connect(mapStateToProps, { startWatchingRoom, stopWatchingRoom })(Room);
