3e57db05b50cb7f5643b5c51a008c3677c36a023
Elias Meire <me@eliasmei.re>
2017-04-03 03:05:45 +0200

    new history api

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App/App';
 import './index.scss';
-import store from './redux/store';
+import store, { history } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App history={history} />
   </Provider>,
   document.getElementById('root')
 );
