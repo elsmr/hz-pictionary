52b0c066546ed0d5131464d27e5ef8f37755f0aa
Elias Meire <me@eliasmei.re>
2017-05-31 03:29:09 +0200

    fix URLs

--- src/client/lib/api.js
+++ src/client/lib/api.js
@@ -1,8 +1,8 @@
 import { ajax } from 'rxjs/observable/dom/ajax';
 import { API_BASE, PROTOCOL } from '../constants';
 
-export const getRoom = name => ajax.getJSON(`${PROTOCOL}://${API_BASE}/rooms/${name}`);
+export const getRoom = name => ajax.getJSON(`${PROTOCOL}://${API_BASE}/api/rooms/${name}`);
 
-export const getRandomWord = () => ajax.getJSON(`${PROTOCOL}://${API_BASE}/words/random`);
+export const getRandomWord = () => ajax.getJSON(`${PROTOCOL}://${API_BASE}/api/words/random`);
 
 export const x = x => x;
