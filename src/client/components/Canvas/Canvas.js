99a564ef87252652e272c35bbf54791ffc949497
Elias Meire <me@eliasmei.re>
2017-05-31 19:46:24 +0200

    more canvas actions

--- src/client/components/Canvas/Canvas.js
+++ src/client/components/Canvas/Canvas.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from 'grommet';
 import DownloadIcon from 'grommet/components/icons/base/Download';
+import RevertIcon from 'grommet/components/icons/base/Revert';
+import RefreshIcon from 'grommet/components/icons/base/Refresh';
 import TurnIndicator from '../TurnIndicator/TurnIndicator';
 import DrawableCanvas from './DrawableCanvas';
 import './Canvas.scss';
@@ -13,7 +15,17 @@ class Canvas extends React.Component {
   }
 
   render() {
-    const { user, drawingPlayer, word, enabled, lines, settings, message } = this.props;
+    const {
+      user,
+      drawingPlayer,
+      word,
+      enabled,
+      lines,
+      settings,
+      message,
+      undoLine,
+      clear,
+    } = this.props;
 
     return (
       <div className="canvas__container">
@@ -28,13 +40,31 @@ class Canvas extends React.Component {
           lines={lines}
           settings={settings}
         />
-        <Button
-          className="button--download"
-          primary
-          icon={<DownloadIcon />}
-          href="#"
-          onClick={e => this.downloadCanvas(e.currentTarget)}
-        />
+        <div className="canvas__actions">
+          {user.id === drawingPlayer.id &&
+            <Button
+              className="button--undo"
+              primary
+              icon={<RevertIcon />}
+              onClick={() => undoLine()}
+            />
+          }
+          {user.id === drawingPlayer.id &&
+            <Button
+              className="button--reset"
+              primary
+              icon={<RefreshIcon />}
+              onClick={() => clear()}
+            />
+          }
+          <Button
+            className="button--download"
+            primary
+            icon={<DownloadIcon />}
+            href="#"
+            onClick={e => this.downloadCanvas(e.currentTarget)}
+          />
+        </div>
         { message &&
           <div className="canvas__message">
             {message}
