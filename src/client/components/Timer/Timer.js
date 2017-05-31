e965e6ce2304e08f0edd0e08f18f0da92206dd6f
Elias Meire <me@eliasmei.re>
2017-05-31 13:20:58 +0200

    time's up message

--- src/client/components/Timer/Timer.js
+++ src/client/components/Timer/Timer.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './Timer.scss';
 
 const getColorForValue = (value, max = 60.0) => {
-  const weighted = (value / max) * 150;
-  if (weighted > 40) {
+  if (value > 20) {
     return '#000';
   }
+  const weighted = (value / max) * 150;
   return `hsl(${weighted}, 100%, 50%)`;
 };
 
 const Timer = ({ seconds }) => (
   <div className="timer" style={{ color: getColorForValue(seconds) }}>
-    {seconds}s left
+    {
+      seconds > 0 ? `${seconds}s left` : 'Time\'s up!'
+    }
+
   </div>
 );
 
