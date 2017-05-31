95bd78fc25ba585f4c4cf47fdebe91e5deb40241
Elias Meire <me@eliasmei.re>
2017-05-31 19:47:17 +0200

    check messages for guesses

--- src/client/redux/modules/chat.js
+++ src/client/redux/modules/chat.js
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/debounceTime';
-import { actionTypes, updateChat } from '../actions';
+import { actionTypes, updateChat, winRound } from '../actions';
 import { CHAT_MAX_LENGTH } from '../../constants';
 
 export const x = 2;
@@ -9,10 +9,27 @@ export const x = 2;
 export const sendMessageEpic = (action$, store) =>
   action$.ofType(actionTypes.sendMessage)
     .debounceTime(200)
-    .mergeMap((action) => {
-      let chat = [...store.getState().room.chat, action.message];
+    .map((action) => {
+      const state = store.getState();
+      const { user, room } = state;
+      const message = action.message;
+      const isDrawingPlayer = user.id === room.game.drawingPlayer.id;
+      let userIdWon = null;
+      if (message.message.indexOf(room.game.currentWord) > -1) {
+        if (isDrawingPlayer) {
+          message.message = '*** I am a stupid cheater ***';
+        } else {
+          userIdWon = user.id;
+        }
+      }
+      let chat = [...room.chat, action.message];
       if (chat.length > CHAT_MAX_LENGTH) {
         chat = chat.slice(Math.max(chat.length - CHAT_MAX_LENGTH, 1));
       }
-      return Observable.of(updateChat(chat));
+
+      return [chat, userIdWon];
+    })
+    .mergeMap(([chat, userIdWon]) => {
+      const actions = userIdWon ? [updateChat(chat), winRound(userIdWon)] : [updateChat(chat)];
+      return Observable.of(...actions);
     });
